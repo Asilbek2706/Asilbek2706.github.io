ed1417dd78d0dfdbb3dce8bc00aa2cdd292ddfa6
Asilbek2706 <asilbekkaromatov2@gmail.com>
2026-01-01 20:14:32 +0500

    Fixed styles

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -16,3 +16,15 @@ export const initProgressBars = () => {
 
     progressBars.forEach(bar => observer.observe(bar));
 };
+
+// Toggler bosilganda body'ni qulflash
+document.querySelector('.custom-toggler').addEventListener('click', function() {
+    document.body.classList.toggle('no-scroll');
+});
+
+// Menyu ichidagi link bosilganda body scrollni qaytarish
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('no-scroll');
+    });
+});
