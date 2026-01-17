ff82ad57c9c2c63a3bb6bfb5c880f9ee17dc810f
Asilbek2706 <asilbekkaromatov2@gmail.com>
2026-01-17 21:53:01 +0500

    Add preloader

--- js/home.js
+++ js/home.js
@@ -32,4 +32,36 @@ const HomeApp = {
     }
 };
 
+function startLoader() {
+    let percentText = document.getElementById('percent-text');
+    let progressCircle = document.querySelector('.circle-progress');
+    let preloader = document.getElementById('preloader');
+
+    let count = 0;
+    let radius = 45;
+    let circumference = 2 * Math.PI * radius; // 282.7
+
+    let interval = setInterval(() => {
+        count++;
+
+        // Foiz matnini yangilash
+        percentText.innerText = count + "%";
+
+        // Aylanani to'ldirish
+        let offset = circumference - (count / 100) * circumference;
+        progressCircle.style.strokeDashoffset = offset;
+
+        if (count >= 100) {
+            clearInterval(interval);
+
+            // Sayt yuklanganini bildirish
+            setTimeout(() => {
+                preloader.classList.add('loaded');
+            }, 500);
+        }
+    }, 30); // 30ms * 100 = taxminan 3 soniya
+}
+
+window.addEventListener('load', startLoader);
+
 document.addEventListener('DOMContentLoaded', () => HomeApp.init());
